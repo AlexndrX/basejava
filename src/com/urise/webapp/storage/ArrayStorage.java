18db7b3c7ebc2df2252d8d5b78d283a380d1b785
Александр Ханжин <alexander.khanzhin@yandex.ru>
2022-07-21 12:29:13 +0300

    create packages

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,35 +1,39 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
-    int count = 0;
+    private int count = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < count; i++) {
             storage[i] = null;
         }
         count = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[count] = r;
         count++;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < count; i++) {
-            if (Objects.equals(storage[i].uuid, uuid)) {
+            if (Objects.equals(storage[i].getUuid(), uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < count; i++) {
             if (storage[i].uuid == uuid) {
 
@@ -46,7 +50,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] fullStorage = new Resume[count];
         for (int i = 0; i < count; i++) {
             fullStorage[i] = storage[i];
@@ -54,7 +58,7 @@ public class ArrayStorage {
         return fullStorage;
     }
 
-    int size() {
+    public int size() {
         return count;
     }
 }
