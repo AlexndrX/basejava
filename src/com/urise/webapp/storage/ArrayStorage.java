1df2aa13c0b5a343523d0be26445e0e9b055cbc7
Александр Ханжин <alexander.khanzhin@yandex.ru>
2022-07-24 10:43:35 +0300

    fix methods clear, update, save, get, delete

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -3,88 +3,67 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
 
     private int count = 0;
 
     public void clear() {
-        Resume[] tempStorage = Arrays.copyOf(storage, count);
-        Arrays.fill(tempStorage, null);
+        Arrays.fill(storage, null);
         count = 0;
     }
 
-    public boolean isExistResume(Resume r) {
-        for (int i = 0; i < count; i++) {
-            if (r.equals(storage[i]) || r.getUuid().equals(storage[i].getUuid())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isExistID(String uuid) {
+    public int findIndex(String uuid) {
         for (int i = 0; i < count; i++) {
             if (uuid.equals(storage[i].getUuid())) {
-                return true;
+                return i;
             }
         }
-        return false;
+        return -1;
     }
 
     public void update(Resume r) {
-        if (!isExistResume(r)) {
+        int index = findIndex(r.getUuid());
+        if (index >= 0) {
+            storage[index] = r;
+        } else if (index == -1){
             System.out.println("Error: this resume with " + r.getUuid() + " not present");
         }
     }
 
     public void save(Resume r) {
-
-        boolean isComplete = false;
-        if (count == 10000) {
-            isComplete = true;
-        }
-
-        if (!isExistResume(r) && !isComplete) {
+        if (count == STORAGE_LIMIT){
+            System.out.println("Error: storage is complete");
+        } else if (findIndex(r.getUuid()) >=0) {
+            System.out.println("Error: this resume with " + r.getUuid() + " is present");
+        } else {
             storage[count] = r;
             count++;
-        } else {
-            System.out.println("Error: this resume with " + r.getUuid() + " is present or storage is complete");
         }
     }
 
     public Resume get(String uuid) {
-        if (isExistID(uuid)) {
-            for (int i = 0; i < count; i++) {
-                if (Objects.equals(storage[i].getUuid(), uuid)) {
-                    return storage[i];
-                }
-            }
+        int index = findIndex(uuid);
+        if (index == -1){
+            System.out.println("Error: this resume not present");
+            return null;
         }
-        System.out.println("Error: this resume not present");
-        return null;
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        if (isExistID(uuid)) {
-            for (int i = 0; i < count; i++) {
-                if (storage[i].uuid == uuid) {
-
-                    for (int j = i + 1; j < count; j++) {
-                        storage[j - 1] = storage[j];
-                    }
-                    storage[count - 1] = null;
-                    count--;
-                    break;
-                }
-            }
-        } else {
+        int index = findIndex(uuid);
+        if (index == -1){
             System.out.println("Error: this resume with uuid: " + uuid + " not present");
+        } else {
+            storage[index] = storage[count - 1];
+            storage[count - 1] = null;
+            count--;
         }
     }
 
