e55735f9ddc7ec46830f90988dfaa60f8d743fe2
Александр Ханжин <alexander.khanzhin@yandex.ru>
2022-10-03 21:09:11 +0300

    implement AbstractPathStorage

--- src/com/urise/webapp/storage/AbstractPathStorage.java
+++ src/com/urise/webapp/storage/AbstractPathStorage.java
@@ -3,23 +3,22 @@ package com.urise.webapp.storage;
 import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public abstract class AbstractPathStorage extends AbstractStorage<Path>{
+public abstract class AbstractPathStorage extends AbstractStorage<Path> {
 
     private Path directory;
 
     protected AbstractPathStorage(String dir) {
         directory = Paths.get(dir);
         Objects.requireNonNull(directory, "Directory must not be null");
-        if (!Files.isDirectory(directory) || !Files.isWritable(directory)){
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
             throw new IllegalArgumentException(dir + " is not directory or is not writable");
         }
     }
@@ -35,43 +34,78 @@ public abstract class AbstractPathStorage extends AbstractStorage<Path>{
 
     @Override
     protected List<Resume> doCopyAll() {
-        return null;
+        if (directory == null) {
+            throw new StorageException("Directory is null", null);
+        }
+        try {
+            List<String> list = Files.readAllLines(directory);
+            List<Resume> resumeList = new ArrayList<>();
+            for (String s : list) {
+                resumeList.add(doGet(Paths.get(s)));
+            }
+            return resumeList;
+        } catch (IOException e) {
+            throw new StorageException("IO error", null);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        if (Files.notExists(directory)) {
+            throw new StorageException("Directory is null", null);
+        }
+        try {
+            return (int) Files.list(directory).count();
+        } catch (IOException e) {
+            throw new StorageException("IO Error", directory.getFileName().toString(), e);
+        }
     }
 
     @Override
     protected Path findSearchKey(String uuid) {
-        return null;
-    }
-
-
-    @Override
-    protected Resume doGet(Path searchKey) {
-        return null;
+        return Paths.get(uuid);
     }
 
     @Override
-    protected void doUpdate(Path searchKey, Resume r) {
-
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(Files.newInputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("IO Error", directory.getFileName().toString(), e);
+        }
     }
 
     @Override
-    protected void doSave(Path searchKey, Resume r) {
-
+    protected void doUpdate(Path path, Resume r) {
+        try {
+            doWrite(new BufferedOutputStream(Files.newOutputStream(path)), r);
+        } catch (IOException e) {
+            throw new StorageException("IO Error", directory.getFileName().toString(), e);
+        }
     }
 
     @Override
-    protected void doDelete(Path searchKey) {
-
+    protected void doSave(Path path, Resume r) {
+        try {
+            Files.createFile(path);
+            doWrite(new BufferedOutputStream(Files.newOutputStream(path)), r);
+        } catch (IOException e) {
+            throw new StorageException("IO Error", directory.getFileName().toString(), e);
+        }
     }
 
     @Override
-    protected boolean isExist(Path searchKey) {
-        return false;
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null, e);
+        }
+    }
+
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.exists(path);
     }
 
     protected abstract void doWrite(OutputStream os, Resume r) throws IOException;
--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -18,6 +18,7 @@ import static com.urise.webapp.storage.ResumeTestData.createResume;
 public abstract class AbstractStorageTest {
 
     protected static final File STORAGE_DIR = new File("C:\\Users\\Professional\\IdeaProjects\\basejava\\storage");
+    protected static final String STORAGE_PATH = "C:\\Users\\Professional\\IdeaProjects\\basejava\\storage\\";
 
     protected final Storage storage;
     private static final String UUID_1 = "uuid1";
